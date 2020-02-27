e42ed4e22c065d3c5ff1a658fdc368449bf0726d
delabroj <delabroj@users.noreply.github.com>
2020-02-27 16:58:34 -0700

    -

--- src/index.tsx
+++ src/index.tsx
@@ -19,23 +19,16 @@ interface Action {
 
 const initialState: State = { count: 0 };
 
-const reducer: React.Reducer<State, Action> = (state, action) => {
-    switch (action.type) {
-        case ActionType.Increment:
-            return { count: state.count + action.payload.count };
-        case ActionType.Decrement:
-            return { count: state.count - action.payload.count };
-    }
-};
-
 const App = () => {
+    const [state, dispatch] = React.useReducer<React.Reducer<State, Action>>(reducer, initialState);
+
     return (
         <div>
-            <div>Count: 0</div>
-            <button>+5</button>
-            <button>+1</button>
-            <button>-1</button>
-            <button>-5</button>
+            <div>Count: {state.count}</div>
+            <button onClick={() => dispatch({ type: ActionType.Increment, payload: { count: 5 } })}>+5</button>
+            <button onClick={() => dispatch({ type: ActionType.Increment, payload: { count: 1 } })}>+1</button>
+            <button onClick={() => dispatch({ type: ActionType.Decrement, payload: { count: 1 } })}>-1</button>
+            <button onClick={() => dispatch({ type: ActionType.Decrement, payload: { count: 5 } })}>-5</button>
         </div>
     );
 };
